06782d4f446c715592b92368dc7182a41a60d16b
Nick Papyan <nickpapyan@icloud.com>
2023-05-11 22:07:24 -0400

    Added git-commentary plugin

--- lua/np/packer.lua
+++ lua/np/packer.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function(use)
   use('ThePrimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('tpope/vim-commentary')
 
   -- Debugging
   use 'mfussenegger/nvim-dap'
